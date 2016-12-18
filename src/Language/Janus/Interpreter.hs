12d00f69c45de98c5b970c92b7de54486165fc7e
Marek Kaput <marek.kaput@outlook.com>
2016-12-18 16:29:16 +0100

    cleanup

--- src/Language/Janus/Interpreter.hs
+++ src/Language/Janus/Interpreter.hs
@@ -52,81 +52,89 @@ class Evaluable a where
 instance Evaluable Val where
   eval = return
 
+instance Evaluable EvalError where
+  eval = throwError
+
 instance Evaluable Expr where
   eval (LiteralExpr a) = eval a
 
-  eval (BlockExpr block) = throwError $ InternalError "not implemented yet"
+  eval (BlockExpr block) = iie "not implemented yet"
 
   eval (ParenExpr e) = eval e
 
-  eval (IndexExpr e idx) = throwError $ InternalError "not implemented yet"
+  eval (IndexExpr e idx) = iie "not implemented yet"
 
-  eval (CallExpr e args) = throwError $ InternalError "not implemented yet"
+  eval (CallExpr e args) = iie "not implemented yet"
 
-  eval (PostfixIncExpr e) = throwError $ InternalError "not implemented yet"
+  eval (PostfixIncExpr e) = iie "not implemented yet"
 
-  eval (PostfixDecExpr e) = throwError $ InternalError "not implemented yet"
+  eval (PostfixDecExpr e) = iie "not implemented yet"
 
-  eval (NotExpr e) = throwError $ InternalError "not implemented yet"
+  eval (NotExpr e) = iie "not implemented yet"
 
-  eval (BitNotExpr e) = throwError $ InternalError "not implemented yet"
+  eval (BitNotExpr e) = iie "not implemented yet"
 
-  eval (PlusExpr e) = throwError $ InternalError "not implemented yet"
+  eval (PlusExpr e) = iie "not implemented yet"
 
-  eval (NegExpr e) = throwError $ InternalError "not implemented yet"
+  eval (NegExpr e) = iie "not implemented yet"
 
-  eval (PrefixIncExpr e) = throwError $ InternalError "not implemented yet"
+  eval (PrefixIncExpr e) = iie "not implemented yet"
 
-  eval (PrefixDecExpr e) = throwError $ InternalError "not implemented yet"
+  eval (PrefixDecExpr e) = iie "not implemented yet"
 
-  eval (ExpExpr a b) = throwError $ InternalError "not implemented yet"
+  eval (ExpExpr a b) = iie "not implemented yet"
 
-  eval (MulExpr a b) = throwError $ InternalError "not implemented yet"
+  eval (MulExpr a b) = iie "not implemented yet"
 
-  eval (DivExpr a b) = throwError $ InternalError "not implemented yet"
+  eval (DivExpr a b) = iie "not implemented yet"
 
-  eval (RemExpr a b) = throwError $ InternalError "not implemented yet"
+  eval (RemExpr a b) = iie "not implemented yet"
 
-  eval (AddExpr a b) = throwError $ InternalError "not implemented yet"
+  eval (AddExpr a b) = iie "not implemented yet"
 
-  eval (SubExpr a b) = throwError $ InternalError "not implemented yet"
+  eval (SubExpr a b) = iie "not implemented yet"
 
-  eval (LshExpr a b) = throwError $ InternalError "not implemented yet"
+  eval (LshExpr a b) = iie "not implemented yet"
 
-  eval (RshExpr a b) = throwError $ InternalError "not implemented yet"
+  eval (RshExpr a b) = iie "not implemented yet"
 
-  eval (BitAndExpr a b) = throwError $ InternalError "not implemented yet"
+  eval (BitAndExpr a b) = iie "not implemented yet"
 
-  eval (BitXorExpr a b) = throwError $ InternalError "not implemented yet"
+  eval (BitXorExpr a b) = iie "not implemented yet"
 
-  eval (BitOrExpr a b) = throwError $ InternalError "not implemented yet"
+  eval (BitOrExpr a b) = iie "not implemented yet"
 
-  eval (EqExpr a b) = throwError $ InternalError "not implemented yet"
+  eval (EqExpr a b) = iie "not implemented yet"
 
-  eval (NeqExpr a b) = throwError $ InternalError "not implemented yet"
+  eval (NeqExpr a b) = iie "not implemented yet"
 
-  eval (LtExpr a b) = throwError $ InternalError "not implemented yet"
+  eval (LtExpr a b) = iie "not implemented yet"
 
-  eval (GtExpr a b) = throwError $ InternalError "not implemented yet"
+  eval (GtExpr a b) = iie "not implemented yet"
 
-  eval (LtEqExpr a b) = throwError $ InternalError "not implemented yet"
+  eval (LtEqExpr a b) = iie "not implemented yet"
 
-  eval (GtEqExpr a b) = throwError $ InternalError "not implemented yet"
+  eval (GtEqExpr a b) = iie "not implemented yet"
 
-  eval (AndExpr a b) = throwError $ InternalError "not implemented yet"
+  eval (AndExpr a b) = iie "not implemented yet"
 
-  eval (OrExpr a b) = throwError $ InternalError "not implemented yet"
+  eval (OrExpr a b) = iie "not implemented yet"
 
-  eval IfExpr{ifCond=cond, ifBranch=a, elseBranch=b} =
-    throwError $ InternalError "not implemented yet"
+  eval IfExpr{ifCond=cond, ifBranch=a, elseBranch=b} = iie "not implemented yet"
 
-  eval WhileExpr{whileCond=cond, whileBody=body} =
-    throwError $ InternalError "not implemented yet"
+  eval WhileExpr{whileCond=cond, whileBody=body} = iie "not implemented yet"
 
-  eval (LoopExpr e) = throwError $ InternalError "not implemented yet"
+  eval (LoopExpr e) = iie "not implemented yet"
 
-  eval BreakExpr = throwError $ InternalError "not implemented yet"
+  eval BreakExpr = iie "not implemented yet"
 
-  eval ContinueExpr = throwError $ InternalError "not implemented yet"
+  eval ContinueExpr = iie "not implemented yet"
 
-  eval (ReturnExpr e) = throwError $ InternalError "not implemented yet"
+  eval (ReturnExpr e) = iie "not implemented yet"
+
+
+--
+-- Misc
+--
+iie :: String -> InterpM a
+iie = throwError . InternalError
