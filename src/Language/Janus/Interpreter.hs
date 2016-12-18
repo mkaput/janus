32df5d2d6e8e4ee5336fdcf6008ecfeaec292f13
Marek Kaput <marek.kaput@outlook.com>
2016-12-18 17:06:16 +0100

    add prims to emphasise that these variables are unevaluated

--- src/Language/Janus/Interpreter.hs
+++ src/Language/Janus/Interpreter.hs
@@ -56,81 +56,81 @@ instance Evaluable EvalError where
   eval = throwError
 
 instance Evaluable Expr where
-  eval (LiteralExpr a) = eval a
+  eval (LiteralExpr a') = eval a'
 
-  eval (BlockExpr block) = iie "not implemented yet"
+  eval (BlockExpr block') = iie "not implemented yet"
 
-  eval (ParenExpr e) = eval e
+  eval (ParenExpr e') = eval e'
 
-  eval (IndexExpr e idx) = iie "not implemented yet"
+  eval (IndexExpr e' idx') = iie "not implemented yet"
 
-  eval (CallExpr e args) = iie "not implemented yet"
+  eval (CallExpr e' args') = iie "not implemented yet"
 
-  eval (PostfixIncExpr e) = iie "not implemented yet"
+  eval (PostfixIncExpr e') = iie "not implemented yet"
 
-  eval (PostfixDecExpr e) = iie "not implemented yet"
+  eval (PostfixDecExpr e') = iie "not implemented yet"
 
-  eval (NotExpr e) = iie "not implemented yet"
+  eval (NotExpr e') = iie "not implemented yet"
 
-  eval (BitNotExpr e) = iie "not implemented yet"
+  eval (BitNotExpr e') = iie "not implemented yet"
 
-  eval (PlusExpr e) = iie "not implemented yet"
+  eval (PlusExpr e') = iie "not implemented yet"
 
-  eval (NegExpr e) = iie "not implemented yet"
+  eval (NegExpr e') = iie "not implemented yet"
 
-  eval (PrefixIncExpr e) = iie "not implemented yet"
+  eval (PrefixIncExpr e') = iie "not implemented yet"
 
-  eval (PrefixDecExpr e) = iie "not implemented yet"
+  eval (PrefixDecExpr e') = iie "not implemented yet"
 
-  eval (ExpExpr a b) = iie "not implemented yet"
+  eval (ExpExpr a' b') = iie "not implemented yet"
 
-  eval (MulExpr a b) = iie "not implemented yet"
+  eval (MulExpr a' b') = iie "not implemented yet"
 
-  eval (DivExpr a b) = iie "not implemented yet"
+  eval (DivExpr a' b') = iie "not implemented yet"
 
-  eval (RemExpr a b) = iie "not implemented yet"
+  eval (RemExpr a' b') = iie "not implemented yet"
 
-  eval (AddExpr a b) = iie "not implemented yet"
+  eval (AddExpr a' b') = iie "not implemented yet"
 
-  eval (SubExpr a b) = iie "not implemented yet"
+  eval (SubExpr a' b') = iie "not implemented yet"
 
-  eval (LshExpr a b) = iie "not implemented yet"
+  eval (LshExpr a' b') = iie "not implemented yet"
 
-  eval (RshExpr a b) = iie "not implemented yet"
+  eval (RshExpr a' b') = iie "not implemented yet"
 
-  eval (BitAndExpr a b) = iie "not implemented yet"
+  eval (BitAndExpr a' b') = iie "not implemented yet"
 
-  eval (BitXorExpr a b) = iie "not implemented yet"
+  eval (BitXorExpr a' b') = iie "not implemented yet"
 
-  eval (BitOrExpr a b) = iie "not implemented yet"
+  eval (BitOrExpr a' b') = iie "not implemented yet"
 
-  eval (EqExpr a b) = iie "not implemented yet"
+  eval (EqExpr a' b') = iie "not implemented yet"
 
-  eval (NeqExpr a b) = iie "not implemented yet"
+  eval (NeqExpr a' b') = iie "not implemented yet"
 
-  eval (LtExpr a b) = iie "not implemented yet"
+  eval (LtExpr a' b') = iie "not implemented yet"
 
-  eval (GtExpr a b) = iie "not implemented yet"
+  eval (GtExpr a' b') = iie "not implemented yet"
 
-  eval (LtEqExpr a b) = iie "not implemented yet"
+  eval (LtEqExpr a' b') = iie "not implemented yet"
 
-  eval (GtEqExpr a b) = iie "not implemented yet"
+  eval (GtEqExpr a' b') = iie "not implemented yet"
 
-  eval (AndExpr a b) = iie "not implemented yet"
+  eval (AndExpr a' b') = iie "not implemented yet"
 
-  eval (OrExpr a b) = iie "not implemented yet"
+  eval (OrExpr a' b') = iie "not implemented yet"
 
-  eval IfExpr{ifCond=cond, ifBranch=a, elseBranch=b} = iie "not implemented yet"
+  eval IfExpr{ifCond=cond', ifBranch=a', elseBranch=b'} = iie "not implemented yet"
 
-  eval WhileExpr{whileCond=cond, whileBody=body} = iie "not implemented yet"
+  eval WhileExpr{whileCond=cond', whileBody=body'} = iie "not implemented yet"
 
-  eval (LoopExpr e) = iie "not implemented yet"
+  eval (LoopExpr e') = iie "not implemented yet"
 
   eval BreakExpr = iie "not implemented yet"
 
   eval ContinueExpr = iie "not implemented yet"
 
-  eval (ReturnExpr e) = iie "not implemented yet"
+  eval (ReturnExpr e') = iie "not implemented yet"
 
 
 --
