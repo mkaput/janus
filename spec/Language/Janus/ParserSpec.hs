7acf1b699fa7d2fbf01b4d1c93d3ef4dc0041f72
Marek Kaput <marek.kaput@outlook.com>
2017-01-25 18:23:41 +0100

    cleanup test

--- spec/Language/Janus/ParserSpec.hs
+++ spec/Language/Janus/ParserSpec.hs
@@ -1,15 +1,14 @@
 module Language.Janus.ParserSpec where
 
-import Test.Hspec
-import Test.QuickCheck
+import           Test.Hspec
+import           Test.QuickCheck
 
-import Language.Janus.AST
-import Language.Janus.Parser
+import           Language.Janus.AST
+import           Language.Janus.Parser
 
 main = hspec spec
 
 spec = do
-  describe "Statements" $ do
-
-    it "letDecl" $
-      parseStatement "let ala = 2;" == Right (LetDecl "ala" (LiteralExpr (JInt 2)))
+  describe "statements" $ do
+    it "let decl" $
+      parseStatement "let ala = 2;" `shouldBe` Right (LetDecl "ala" (LiteralExpr (JInt 2)))
